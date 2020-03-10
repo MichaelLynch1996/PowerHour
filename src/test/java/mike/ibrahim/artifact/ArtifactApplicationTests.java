231e7462190e42d73fdc12cb3e419159b368d04c
Michael Lynch <40207207+MichaelLynch1996@users.noreply.github.com>
2020-03-10 15:55:43 -0400

    spring init

---
+++ src/test/java/mike/ibrahim/artifact/ArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package mike.ibrahim.artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
